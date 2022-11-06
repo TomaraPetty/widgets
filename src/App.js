28717d65f296b362f115a91ebb3b446e1237d3cd
Tomara Petty <tomararuth@gmail.com>
2022-11-05 18:03:10 -0700

    created dropdown component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
 const items = [
   {
-    title: "What is React?",
-    content: "React is a front end javascript framework"
+    title: 'What is React?',
+    content: 'React is a front end javascript framework',
   },
   {
-    title: "Why react?",
-    content: "React is a favorite JS library among engineers"
+    title: 'Why react?',
+    content: 'React is a favorite JS library among engineers',
   },
   {
-    title: "How do you use React",
-    content: "You use react by creating components"
-  }
+    title: 'How do you use React',
+    content: 'You use react by creating components',
+  },
+];
+
+const options = [
+  {
+    label: 'The Color Red',
+    value: 'red',
+  },
+  {
+    label: 'The Color Green',
+    value: 'green',
+  },
+  {
+    label: 'A Shade Of Blue',
+    value: 'blue',
+  },
 ];
 
 export default () => {
-  return <div>
-    {/* <Accordion items={items} /> */}
-    <Search />
-  </div>;
+  return (
+    <div>
+      {/* <Accordion items={items} /> */}
+      {/* <Search /> */}
+      <Dropdown options={options} />
+    </div>
+  );
 };
